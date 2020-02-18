fd4edf6c98e7e0a33e6f000739b368843e9d219f
Niklas Bönisch <niklas.boenisch@stud.tu-darmstadt.de>
2020-02-18 21:35:56 +0100

    captcha wird geladen

--- pr0-crawler.py
+++ pr0-crawler.py
@@ -6,5 +6,29 @@ Created on Thu Jan 23 19:29:43 2020
 """
 
 import requests
+import base64
+from PIL import Image
+from io import BytesIO
+
+
+def main():
+    getCaptcha()
+def connect():
+    return
+
+def getPost(postID):
+    return
 
 def getCaptcha():
+    req = requests.get("https://pr0gramm.com/api/user/captcha")
+    if req.status_code == 200:
+        token = req.json()
+        captcha = req.json().get("captcha")
+        png_captcha = Image.open(BytesIO(base64.b64decode(captcha.split(",")[1])))
+        png_captcha.show()
+    else:
+        raise
+        
+
+if __name__ == "__main__":
+    main()
